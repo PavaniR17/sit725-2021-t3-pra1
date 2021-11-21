31876753286cec8fd0e806373b6f744c64a9fc01
Pavani R <praavi@deakin.edu.au>
2021-11-21 22:15:39 +1100

    made few changes to html and css

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-'use strict'
-   
-const switcher = document.querySelector('.btn');
-
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme')
-
-    var className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark";
-    }
-    else {
-        this.textContent = "Light";
-    }
-
-    console.log('current class name: ' + className);
-
-});
+function myFunction() {
+    document.getElementById("heading").style.fontSize = "25px"; 
+    document.getElementById("heading").style.color = "red";
+    document.getElementById("heading").style.backgroundColor = "yellow";        
+  }
